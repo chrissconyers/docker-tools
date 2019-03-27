67144481dd71d642ef2832a5b0d974e6aba049c8
Chris Sconyers <48865772+chrissconyers@users.noreply.github.com>
2019-03-27 16:08:12 -0400

    add X forwarding command line option that mounts the local X server socket

--- pipenv/docker-pipenv.sh
+++ pipenv/docker-pipenv.sh
@@ -4,6 +4,12 @@ export PUID=$(id -u)
 export PGID=$(id -g)
 export WORKING_DIR=$PWD
 
+X_FORWARD=""
+if [[ -n $1 ]] && [[ $1 == '-X' ]]; then
+  X_FORWARD="-v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY"
+  shift
+fi
+
 cd "$(dirname "$(readlink -f "$0")")"
-docker-compose run pipenv pipenv "$@"
+docker-compose run $X_FORWARD pipenv pipenv "$@"
 
